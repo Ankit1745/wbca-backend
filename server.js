b747e42c45ec05f1315061cc9eb3f9543d697007
Ankit1745 <ankitvis787966@gmail.com>
2025-02-16 12:41:08 +0530

    cors config

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('Connection Error:', err));
+
+
 
 // Routes
 app.use('/api/register/assessor', require('./routes/assessorRoutes'));
